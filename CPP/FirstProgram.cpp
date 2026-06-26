11240644130e3b20f1cb6b1787989f818ce5bd73
exxario <4126akah@gmail.com>
2026-06-26 08:55:21 +0530

    Remaned C++ and Use Comments and 'endl' in the program

---
+++ CPP/FirstProgram.cpp
@@ -0,0 +1,16 @@
+#include<iostream> //iostream is a header file here.
+using namespace std; //this is used so that we dont have to write std::cout every time we want to print something.
+
+//this is the main function where the execution of the program begins.
+int main(){
+    //cout is used to print something on the screen.
+    cout<<"Hello World"<<endl;
+
+    //endl is used to print a new line.
+    
+    cout<<"Hello"<<endl<<"World";
+    
+    //this is used to return a value from the main function.
+    return 0;
+
+}
