40a9908fdeb085162a2133e1b5563b804b3d209f
exxario <4126akah@gmail.com>
2026-06-27 13:04:11 +0530

    input/output program in C++

---
+++ CPP/input_output.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    char urFirstLetter;
+    cout<<"enter your first letter: ";
+    cin>>urFirstLetter;
+    cout<<"your first letter is: "<<urFirstLetter<<endl;
+
+    int crewMembers;
+    cout<<"enter the number of crew members: ";
+    cin>>crewMembers;
+    cout<<"number of crew members: "<<crewMembers<<endl;
+
+    string crewName;
+    cout<<"enter the name of the crew: ";
+    cin>>crewName;
+    cout<<"name of the crew: "<<crewName<<endl;
+
+    return 0;
+
+}
