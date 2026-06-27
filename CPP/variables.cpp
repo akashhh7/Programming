cd67dcfced70c1e51428e0469e56adcaecbba290
exxario <4126akah@gmail.com>
2026-06-27 12:15:36 +0530

    added variables

---
+++ CPP/variables.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int numberOfPirates = 10;
+    cout<<"Number of pirates: "<<numberOfPirates<<endl;
+
+    char firstLetterOfShip = 'G';
+    cout<<"First letter of ship: "<<firstLetterOfShip<<endl;
+
+    float priceOfBerries = 4.99;
+    cout<<"Price of berries: "<<priceOfBerries<<endl;
+
+    double priceOfShip = 299.845;
+    cout<<"Price of the ship: "<<priceOfShip<<endl;
+
+    string pirateKing = "Luffy";
+    cout<<"Pirate King: "<<pirateKing<<endl;
+    
+    bool luffyGonnaBePirateKing = true;
+    cout<<"Luffy gonna be the pirate king (true/false): "<<luffyGonnaBePirateKing<<endl;
+
+    bool blackbeardGonnaBePirateKing = false;
+    cout<<"Blackbeard gonna be the pirate king (true/false): "<<blackbeardGonnaBePirateKing<<endl;
+
+    return 0;
+}
